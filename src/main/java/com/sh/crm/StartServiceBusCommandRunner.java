2a1bb3517704457d15f11328f7b910279bb9392f
Ahmed Shaheen <achaheen@gmail.com>
2019-04-09 14:45:11 +0300

    Dynamic fields

--- src/main/java/com/sh/crm/StartServiceBusCommandRunner.java
+++ src/main/java/com/sh/crm/StartServiceBusCommandRunner.java
@@ -6,6 +6,7 @@ import com.sh.crm.services.notification.Config;
 import com.sh.crm.services.notification.consumer.NotificationConsumer;
 import com.sh.crm.services.notification.service.impl.NotificationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -29,12 +30,36 @@ public class StartServiceBusCommandRunner implements CommandLineRunner {
     @Autowired
     private TicketsRepo ticketsRepo;
 
+    @Value("${mw.ssl.key-store}")
+    private String keyStore;
+
+    @Value("${mw.ssl.key-store-password}")
+    private String keyStorePassword;
+
     @Override
     public void run(String... args) throws Exception {
+        setSystemVariable();
         eventBus.on( $( "NC" ), notificationConsumer );
+
         // notificationService.formatTemplate( 1, 19011500016l, "" );
         //  Ticket ticket = ticketsRepo.findById( Long.parseLong( "18121600001" ) ).orElse( null );
 
         //  System.out.println( "ticket : " + ticket );
+    }
+
+    private void setSystemVariable() {
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+
+            System.setProperty( "javax.net.ssl.trustStore",
+                    classLoader.getResource( keyStore ).getFile() );
+            System.setProperty( "javax.net.ssl.trustStorePassword",
+                    keyStorePassword );
+            System.setProperty( "javax.xml.bind.JAXBContext", "com.sun.xml.internal.bind.v2.ContextFactory" );
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
     }
 }
